ffb159b0bfb5736bc5204aba660f02f2db7751fc
Rayane-Mayara <rayane.malaquias@arapiraca.ufal.br>
2025-10-22 16:01:35 -0300

    Menu inicial
    Fix syntax errors and improve menu functionality.

--- JogoDaVelha.c
+++ JogoDaVelha.c
@@ -1,6 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void mostrar_menu();
+void iniciar_jogo();
+void mostrar_placar();
 
 int main() {
     printf("Jogo da Velha\n");
+   int opção;
+
+    While (1) {
+        mostrar_menu ();
+        printf("escolha uma opção: ");
+        if (scanf("%d", &opção) != 1) {
+            printf("Opção inválida. Tente novamente! ");
+            while (getchar() != '\n');
+            continue;
+        }
+        switch (opção) (
+            case 1:
+                iniciar_jogo(); 
+                break;
+            case 2:
+                mostrar_placar(); 
+                break;
+            case 3: 
+                printf("\nTem certeza que deseja sair? (1 para Sim / 0 para Não): ");
+                int confirmacao;
+                scanf("%d", &confirmacao);
+                if (confirmacao == 1) {
+                    printf("Programa finalizado. Até mais!\n"); 
+                    return 0;
+                }
+                break;
+            default:
+                printf("\nOpção inválida. Por favor, escolha 1, 2 ou 3.\n\n");
+                break;
+    }
     return 0;
+}
+
+void mostrar_menu() { 
+    printf("=======================================\n");
+    printf("        JOGO DA VELHA EM C\n");
+    printf("=======================================\n");
+    printf("1. Iniciar Jogo\n");
+    printf("2. Placar\n");
+    printf("3. Sair\n");
+    printf("---------------------------------------\n");
+}
+void mostrar_placar() {
+    printf("\n[ Placar ]\n");
+    printf("Vitórias do Humano (X): %d\n", vitorias_humano);
+    printf("Vitórias da Máquina (O): %d\n", vitorias_maquina);
+    printf("Empates: %d\n", empates);
+    printf("\n");
+}
+void iniciar_jogo() {
+    printf("\n[ O Jogo vai começar!\n");
 }
